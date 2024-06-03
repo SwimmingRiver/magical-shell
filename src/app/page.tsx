9082bb734a107651f4fc155edecd5b0aa6336645
swimmingRiver <swimmingr@gmail.com>
2024-06-03 18:41:44 +0900

    🍱 Settings: logo 이미지 추가
    settings: logo 이미지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <div>hi</div>;
+  return (
+    <div style={{ backgroundColor: '#a56bdb' }}>
+      <Image src="/images/sea-snail.png" alt="logo" width={500} height={500} />
+    </div>
+  );
 }
