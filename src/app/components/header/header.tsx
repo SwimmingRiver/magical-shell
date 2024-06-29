0c427c2ad5c88feecd2e993fdeb794f63e8f05ee
swimmingRiver <swimmingr@gmail.com>
2024-06-29 15:01:59 +0900

    🔥 Styles: 메뉴 버튼 제거
    styles: 메뉴 버튼 제거

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Image from 'next/image';
 import styles from './header.module.css';
 
 function Header() {
   return (
     <div className={styles.header}>
       <div>MAGICAL-SHELL</div>
-      <div className={styles.menu}>
-        <Image src="/images/menu.svg" alt="menu" width={30} height={30} />
-      </div>
     </div>
   );
 }
