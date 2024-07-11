e13a25564713a072c66e2393505405ea79580a7f
swimmingRiver <swimmingr@gmail.com>
2024-07-11 16:19:07 +0900

    📦️ Feat: 입력 창 추가
    feat: 입력 창 추가

---
+++ src/app/components/messageList/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './page.module.css';
+
+function MessageList() {
+  return <div className={styles.messageList}>MessageList</div>;
+}
+
+export default MessageList;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,33 @@
 'use client';
 
+import { useState } from 'react';
+import MessageList from './components/messageList/page';
 import styles from './page.module.css';
 
 export default function Home() {
+  const [inputData, setInputData] = useState('');
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    setInputData('');
+  };
   return (
     <div className={styles.mainWrapper}>
-      <div>hi</div>
+      <div className={styles.messageWrapper}>
+        <MessageList />
+        <div className={styles.inputWrapper}>
+          <form onSubmit={submitHandler}>
+            <input
+              value={inputData}
+              className={styles.input}
+              placeholder="무엇이든지 물어보세요"
+              onChange={(e) => {
+                setInputData(e.target.value);
+              }}
+            />
+            <button type="button">send</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
